1c5ff7e745733f51a0c1d5f70deba98becc3a7a8
lukematyja <luke.matyja@gmail.com>
2023-10-19 15:21:05 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+/ script.js
+// You can add JavaScript functionality here if needed
+// For example, you can add interactivity to the page, fetch dynamic data, etc.
+To use these CSS and JavaScript files, you would include them in your HTML pages like this:
